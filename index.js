02f7b5b98f851ceadfef05e688a8bb0cf252c131
thekelvinliu <kelvin@thekelvinliu.com>
2017-01-18 22:20:16 -0600

    created a function via serverless-mocha-plugin

---
+++ index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+import AWS from 'aws-sdk';
+import Promise from 'bluebird';
+import Random from 'random-js';
+
+const r = new Random(Random.engines.mt19937().autoSeed());
+
+export const hello = async (event, context, callback) => {
+  await Promise.delay(750);
+  const message = `your random number is ${r.integer(1, 10)}`;
+  callback(null, {
+    statusCode: 200,
+    body: JSON.stringify({
+      message,
+      input: event
+    })
+  });
+};
