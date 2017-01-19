a98a6749efcccb1e5e2c0d769b5a4fdf981ec2b7
thekelvinliu <kelvin@thekelvinliu.com>
2017-01-18 23:22:39 -0600

    deployable

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 'use strict';
 
-import AWS from 'aws-sdk';
+// import AWS from 'aws-sdk';
 import Promise from 'bluebird';
 import Random from 'random-js';
 
 const r = new Random(Random.engines.mt19937().autoSeed());
 
-export const hello = async (event, context, callback) => {
+export const handler = async (event, context, callback) => {
   await Promise.delay(750);
   const message = `your random number is ${r.integer(1, 10)}`;
   callback(null, {
