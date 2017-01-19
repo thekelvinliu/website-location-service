fc995c8fa2c511f707984da0571208164d952421
thekelvlinliu <kelvin@thekelvinliu.com>
2017-01-19 15:21:46 -0600

    i'm all over the place rn will look later

--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
 'use strict';
 
-// import AWS from 'aws-sdk';
+import bluebird from 'bluebird';
+import fetch from 'node-fetch';
 import Random from 'random-js';
 
-import Bluebird from 'bluebird';
-import fetch from 'node-fetch';
-fetch.Promise = Bluebird;
+// plug bluebird into fetch
+fetch.Promise = bluebird;
 
-fetch('https://jsonplaceholder.typicode.com/users')
-  .then(res => res.json())
-  .then(json => console.log(json));
-
+// setup a random generator
 const r = new Random(Random.engines.mt19937().autoSeed());
 
+async function getJSON(url) {
+  return await fetch(url).then(res => res.json());
+}
+
+let data = getJSON('https://jsonplaceholder.typicode.com/users');
+console.log(data.length);
+// console.log(data[r.integer(0, data.length - 1)]);
+
+// lambda
 export const handler = async (event, context, callback) => {
   await fetch.Promise.delay(750);
   const message = `your random number is ${r.integer(1, 10)}`;
