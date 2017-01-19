f13abef8a8a00bf1bc2333a3a8d0f5bdcd605d8d
thekelvlinliu <kelvin@thekelvinliu.com>
2017-01-19 15:06:18 -0600

    moving things to src

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 'use strict';
 
 // import AWS from 'aws-sdk';
-import Promise from 'bluebird';
 import Random from 'random-js';
+
+import Bluebird from 'bluebird';
+import fetch from 'node-fetch';
+fetch.Promise = Bluebird;
+
+fetch('https://jsonplaceholder.typicode.com/users')
+  .then(res => res.json())
+  .then(json => console.log(json));
 
 const r = new Random(Random.engines.mt19937().autoSeed());
 
 export const handler = async (event, context, callback) => {
-  await Promise.delay(750);
+  await fetch.Promise.delay(750);
   const message = `your random number is ${r.integer(1, 10)}`;
   callback(null, {
     statusCode: 200,
