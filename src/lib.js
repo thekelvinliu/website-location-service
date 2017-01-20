5df65c9689bbd14608ef3c7c488fcf5588e18d4c
thekelvlinliu <kelvin@thekelvinliu.com>
2017-01-19 19:39:28 -0600

    some refactoring and cleaning

--- src/lib.js
+++ src/lib.js
@@ -8,18 +8,10 @@ fetch.Promise = bluebird;
 
 // constants
 export const GEONAMES_URL = 'http://api.geonames.org/findNearbyPlaceNameJSON';
-export const GEONAMES_QUERY = {
-  username: 'kelvinliu',
-  lat: 0,
-  lng: 0
-};
+export const GEONAMES_USER = 'kelvinliu';
 
 // functions
 // returns whether obj contains k
 export const contains = (obj, k) => typeof obj[k] !== 'undefined';
 // returns json from a given url and query
-export const getJSON = async (u, q) => fetch([u, qs.stringify(q)].join('?'))
-  .then(res => res.json())
-  .catch(err => {
-    console.err(err);
-  });
+export const getJSON = async (u, q) => fetch([u, qs.stringify(q)].join('?')).then(res => res.json());
--- src/updateLocation.js
+++ src/updateLocation.js
@@ -5,14 +5,12 @@ import * as lib from './lib.js';
 // lambda function handler
 export const handler = async (event, context, callback) => {
   // load location data from geonames api
-  let data = await lib.getJSON(lib.GEONAMES_URL, lib.GEONAMES_QUERY)
-    // extract geonames property
-    .then(data => data.geonames[0])
-    .catch(err => {
-      console.warn(err);
-    });
-  console.log(!!data);
-  callback(null, JSON.stringify(data));
+  let data = await lib.getJSON(lib.GEONAMES_URL, {
+    username: lib.GEONAMES_USER,
+    lat: 10,
+    lng: '10'
+  });
+  // extract useful information
+  const info = (lib.contains(data, 'geonames')) ? data.geonames[0] : null;
+  callback(null, info);
 };
-
-handler('', '', () => {});
