fc140758ee51c84882121ffd7abee961f6de3a17
thekelvinliu <kelvin@thekelvinliu.com>
2017-01-20 17:03:58 -0600

    making progress i promise

--- src/lib.js
+++ src/lib.js
@@ -13,5 +13,15 @@ export const GEONAMES_USER = 'kelvinliu';
 // functions
 // returns whether obj contains k
 export const contains = (obj, k) => typeof obj[k] !== 'undefined';
+// returns a string error message
+export const errStr = (code, str) => `[${code}] ${str}`;
 // returns json from a given url and query
-export const getJSON = async (u, q) => fetch([u, qs.stringify(q)].join('?')).then(res => res.json());
+export const getJSON = async (u, q) => fetch([u, qs.stringify(q)].join('?'))
+  .then(res => res.json());
+// returns whether obj is empty
+export const isEmpty = obj => {
+  for (const k in obj)
+    if (contains(obj, k))
+      return false;
+  return true;
+};
--- src/updateLocation.js
+++ src/updateLocation.js
@@ -3,14 +3,34 @@
 import * as lib from './lib.js';
 
 // lambda function handler
+// expected to get triggered by a yo location (yolo) via the yo api
 export const handler = async (event, context, callback) => {
-  // load location data from geonames api
-  let data = await lib.getJSON(lib.GEONAMES_URL, {
-    username: lib.GEONAMES_USER,
-    lat: 10,
-    lng: '10'
+  // respond with 400 when request has no params
+  if (lib.isEmpty(event.queryStringParameters)) {
+    const code = 400;
+    const message = lib.errStr(code, 'initial request has no parameters');
+    console.error(message);
+    return callback(null, {
+      statusCode: code,
+      body: JSON.stringify({ message })
+    });
+  }
+  // respond with the params
+  callback(null, {
+    statusCode: 200,
+    body: JSON.stringify(event.queryStringParameters)
   });
-  // extract useful information
-  const info = (lib.contains(data, 'geonames')) ? data.geonames[0] : null;
-  callback(null, info);
+  // // load location data from geonames api
+  // let data = await lib.getJSON(lib.GEONAMES_URL, {
+  //   username: lib.GEONAMES_USER,
+  //   lat: 10,
+  //   lng: '10'
+  // });
+  // // extract useful information
+  // const info = (lib.contains(data, 'geonames')) ? data.geonames[0] : null;
+  // callback(null, {
+  //   statusCode: 200,
+  //   headers: { 'content-type': 'application/json' },
+  //   body: JSON.stringify({ event })
+  // });
 };
